2eabeaefcbefa9098586096cc6e0bef34e7a01cc
DoubleBerserker <harshmawandia03@gmail.com>
2025-09-27 23:32:26 +0530

    chore: setup postgresql db and mapstruct

---
+++ src/main/java/com/DoubleBerserker/Personal/Blog/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.DoubleBerserker.Personal.Blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping({"/", "/home"})
+    public String index() {
+        return "Hello World";
+    }
+}
