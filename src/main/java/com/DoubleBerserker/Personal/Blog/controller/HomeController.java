884ec86ccbcb57e66f7f603c501bfeb0e02be678
DoubleBerserker <harshmawandia03@gmail.com>
2025-12-24 19:32:03 +0530

    add: skeleton page for thymeleaf to auto-populate

--- src/main/java/com/DoubleBerserker/Personal/Blog/controller/HomeController.java
+++ src/main/java/com/DoubleBerserker/Personal/Blog/controller/HomeController.java
@@ -10,9 +10,9 @@ public class HomeController {
     @GetMapping({"/", "/home"})
     public String home(Model model) {
         model.addAttribute("activePage", "home");
-        model.addAttribute("title", "Harsh's Blog");
-//        model.addAttribute("posts")
-        return "homepage";
+        model.addAttribute("title", "Home");
+        model.addAttribute("page", "homepage");
+        return "base";
     }
 
 }
