caf209e4f61f46d2d94de001ce438a43345e796d
DoubleBerserker <harshmawandia03@gmail.com>
2025-12-24 19:49:57 +0530

    add: template and controller for posts and about page

--- src/main/java/com/DoubleBerserker/Personal/Blog/controller/HomeController.java
+++ src/main/java/com/DoubleBerserker/Personal/Blog/controller/HomeController.java
@@ -8,10 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping({"/", "/home"})
-    public String home(Model model) {
+    public String getHomepage(Model model) {
         model.addAttribute("activePage", "home");
         model.addAttribute("title", "Home");
         model.addAttribute("page", "homepage");
+        return "base";
+    }
+
+    @GetMapping("/about")
+    public String getAboutPage(Model model) {
+        model.addAttribute("activePage", "about");
+        model.addAttribute("title", "About");
+        model.addAttribute("page", "about");
         return "base";
     }
 
--- src/main/java/com/DoubleBerserker/Personal/Blog/controller/PostController.java
+++ src/main/java/com/DoubleBerserker/Personal/Blog/controller/PostController.java
@@ -1,11 +1,21 @@
 package com.DoubleBerserker.Personal.Blog.controller;
 
 import com.DoubleBerserker.Personal.Blog.dto.PostDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/posts")
+@Controller
 public class PostController {
+
+    @GetMapping("/posts")
+    public String getPostsMainPage(Model model) {
+        model.addAttribute("activePage", "post");
+        model.addAttribute("title", "Post");
+        model.addAttribute("page", "post");
+        return "base";
+    }
 
     @GetMapping()
     public PostDto getPosts() {
